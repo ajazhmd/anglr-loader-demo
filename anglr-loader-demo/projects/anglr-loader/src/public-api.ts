c6e79f90ca10e4b81d0e366682940285f5f09e53
Ajaz Ahmed <Ajaz.Ahmed@MOTIFWORKS.IN>
2019-12-02 12:41:56 +0530

    Reset

--- anglr-loader-demo/projects/anglr-loader/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of anglr-loader
- */
-
-export * from './lib/anglr-loader.service';
-export * from './lib/anglr-loader.component';
-export * from './lib/anglr-loader.module';
